9329c8e63076212ee84b2a9486633a0d93a43775
Xiangmin Jiao <xmjiao@gmail.com>
2016-05-26 06:46:31 -0400

    Changed path for building msp.

--- util/build_msp.m
+++ util/build_msp.m
@@ -1,11 +1,22 @@
 function build_msp(varargin)
 %build_msp Build script for MSP
 
-% First, compile test scripts
-lines = grep_pattern('crs*.m', '\n%#codegen -args');
-files = regexp(lines, '(\w+.m):', 'tokens');
+mspackroot = fileparts(which('startup_mspack'));
+curpath = pwd;
+cd(mspackroot);
 
-for j=1:length(files)
-    file = files{j}{1};
-    compile('-omp', '-noinf', '-O', varargin{:}, file);
+try
+    % First, compile test scripts
+    lines = grep_pattern('crs*.m', '\n%#codegen -args');
+    files = regexp(lines, '(\w+.m):', 'tokens');
+    
+    for j=1:length(files)
+        file = files{j}{1};
+        compile('-omp', '-noinf', '-O', varargin{:}, file);
+    end
+catch ME
+    cd(curpath);
+    rethrow(ME)
 end
+
+cd(curpath);
